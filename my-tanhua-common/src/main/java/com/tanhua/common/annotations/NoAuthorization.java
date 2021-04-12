2b791820a3b27e6fe0b3678246c527f23046cd2d
isolated <15071340963@163.com>
2021-04-12 12:45:37 +0800

    -- add annotate to NoAuthorization

--- my-tanhua-common/src/main/java/com/tanhua/common/annotations/NoAuthorization.java
+++ my-tanhua-common/src/main/java/com/tanhua/common/annotations/NoAuthorization.java
@@ -2,6 +2,9 @@ package com.tanhua.common.annotations;
 
 import java.lang.annotation.*;
 
+/**
+ * 用于判断是否需要token认证
+ */
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
 @Documented //标记注解
