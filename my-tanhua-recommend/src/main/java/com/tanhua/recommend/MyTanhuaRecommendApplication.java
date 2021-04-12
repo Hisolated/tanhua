ff02dcf45db8c9b6972dd5163a0520d694ce3b02
isolated <15071340963@163.com>
2021-04-12 19:44:48 +0800

    -- init my-tanhua-recommend

---
+++ my-tanhua-recommend/src/main/java/com/tanhua/recommend/MyTanhuaRecommendApplication.java
@@ -0,0 +1,13 @@
+package com.tanhua.recommend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyTanhuaRecommendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyTanhuaRecommendApplication.class, args);
+    }
+
+}
---
+++ my-tanhua-recommend/src/test/java/com/tanhua/recommend/MyTanhuaRecommendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tanhua.recommend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyTanhuaRecommendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
